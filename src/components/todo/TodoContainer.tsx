c26df48385fd674064dce4d8025d8caf728067d6
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-02-08 00:11:31 +0600

    12.10

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -2,15 +2,25 @@ import { clearTodos } from '@/redux/features/todoSlice';
 import AddTodoModal from './AddTodoModal';
 import TodoCard from './TodoCard';
 import TodoFilter from './TodoFilter';
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { useAppDispatch } from '@/redux/hooks';
 import { Button } from '../ui/button';
+import { useGetTodosQuery } from '@/redux/api/api';
 
 const TodoContainer = () => {
   // const [todos, setTodos] = useState({ data: [] });
   const dispatch = useAppDispatch();
 
-  const { todos } = useAppSelector((state) => state.todos);
+  //* From Local state
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  // * From server state
+  const { data: todos, isError, isLoading } = useGetTodosQuery(undefined);
+
   console.log(todos);
+
+  if (isLoading) {
+    return <p>Loading....</p>;
+  }
 
   return (
     <div>
@@ -23,7 +33,7 @@ const TodoContainer = () => {
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]">
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
-          {todos?.map((item) => (
+          {todos?.data.map((item) => (
             <TodoCard key={item.id} {...item} />
           ))}
         </div>
